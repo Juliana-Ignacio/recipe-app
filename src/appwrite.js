5db424021e8a4b237c99d99cb2b664285379089c
Juliana-Ignacio <julianaignacio03@gmail.com>
2025-12-02 20:20:49 +0800

    Use environment variables for Appwrite configurarion

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,9 +1,14 @@
-import { Client, Databases, Storage, ID } from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
-const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")  // your endpoint
-  .setProject("692e75550014928a450b");              // your project ID
+// Initialize the Appwrite client
+const client = new Client();
 
-export const databases = new Databases(client);
-export const storage = new Storage(client);
-export { ID };
+client
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // Your Appwrite endpoint
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT); // Your project ID
+
+// Initialize databases and storage services
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export { client, databases, storage };
